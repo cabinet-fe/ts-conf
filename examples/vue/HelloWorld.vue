09fbb1223d65146b577ace6a3ae1f2e0a2b9aace
whj <emailwen@qq.com>
2025-03-10 15:07:06 +0800

    refactor: 更新依赖

--- examples/vue/HelloWorld.vue
+++ examples/vue/HelloWorld.vue
@@ -1,9 +1,11 @@
 <template>{{ greeter }}: Hello {{ msg }}~</template>
 
 <script lang="ts" setup>
+import { shallowRef } from 'vue'
+
 defineProps({
   msg: String
 })
 
-const greeter = $shallowRef('Hodge')
+const greeter = shallowRef('Hodge')
 </script>
